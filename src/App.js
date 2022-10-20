03f6fd992ceb67fcbdfcc2d44b22b7d1ecb26901
JesusKno <57113540+JesusKno@users.noreply.github.com>
2022-10-20 10:00:03 -0500

    added itemCount 2 function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { ItemDetailContainer } from './containers/ItemDetailContainer/ItemDetailContainer';
 import { ItemListContainer } from './containers/ItemListContainer/itemListContainer';
+import { CustomProvider } from './context/CustomContext';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = ()=> {
@@ -10,16 +11,17 @@ const App = ()=> {
 
   return(
     <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={< ItemListContainer greeting={mensaje}/>} />
-        <Route
-          path='/categoria/:id'
-          element={<ItemListContainer greeting={mensaje}/>} 
-        />
-        <Route path='/producto/:id' element={< ItemDetailContainer/>}/>
-      </Routes>
-      
+      <CustomProvider>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={< ItemListContainer greeting={mensaje}/>} />
+          <Route
+            path='/categoria/:id'
+            element={<ItemListContainer greeting={mensaje}/>} 
+          />
+          <Route path='/producto/:id' element={< ItemDetailContainer/>}/>
+        </Routes>
+      </CustomProvider>    
     </BrowserRouter>
   )
 
--- src/containers/ItemDetail/ItemDetail.js
+++ src/containers/ItemDetail/ItemDetail.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React,  { useState, useContext } from 'react';
+import ItemCount from '../../components/ItemCount/ItemCount';
+import { Context } from '../../context/CustomContext';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom';
 
 const ItemPaper = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -14,18 +17,36 @@ const ItemPaper = styled(Paper)(({ theme }) => ({
 
 const ItemDetail = ({product}) => {
 
+    const [showItemCount, setShowItemCount] = useState(true);
+    const { addItem } = useContext(Context);
+
+    const onAdd = (count) => {
+        addItem(product, count);
+        setShowItemCount(false);
+    };
+
     return (
 
-         <Box sx={{ flexGrow: 1 }}>
-          <Grid containers spacing={2}>
-            <Grid item xs={8}>
+         <Box sx={{ width: '100%' }}>
+          <Grid container direction="row" justifyContent="center" alignItems="center" rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid item xs={6}>
              <ItemPaper> <img alt={product.title} src={product.image}/> </ItemPaper>
+             <h1>{product.title}</h1>
+             <span>{product.description}</span>
+             <h2>${product.price}</h2>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={6}>
              <ItemPaper>
-               <h1>{product.title}</h1>
-               <span>{product.description}</span>
-               <h2>{product.price}</h2>
+                <h4>Aqui ira el Button</h4>
+                {
+                    showItemCount ? (
+                        <ItemCount initial={1} stock={5} onAdd={onAdd} />
+                    ) : (
+                        <Link to={'/cart'}>
+                            <button>Finalizar Compra</button>
+                        </Link>
+                    )
+                }
              </ItemPaper>   
             </Grid>
           </Grid>  
